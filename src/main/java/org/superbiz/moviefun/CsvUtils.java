afb9ec6588818f44f31fa7a4e185c695bf5eb3af
rajaganesh88 <R.Rajasekaran@perficient.com>
2018-12-13 11:07:04 +0530

    Read File system

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -14,7 +14,8 @@ public class CsvUtils {
 
     public static String readFile(String path) {
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            ClassLoader classLoader = Class.forName("org.superbiz.moviefun.CsvUtils").getClassLoader();
+            Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,7 +23,7 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
